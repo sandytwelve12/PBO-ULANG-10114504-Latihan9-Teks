d47e8617e067f4268be633d82c3e9e16943d729f
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:38:34 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan9-Teks/src/pbo/ulang/pkg10114504/latihan9/teks/PBOULANG10114504Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program teks
+ */
+public class PBOULANG10114504Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi" + c);
+                
+    }
+    
+}
